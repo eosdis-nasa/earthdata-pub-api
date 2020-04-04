5c47d748830d756cf50baf1e4c461d68b893d5d4
Brian Ellingson <brianpojo56@gmail.com>
2020-04-04 09:47:22 -0500

    Added gitignore

--- build.sh
+++ build.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
 
-#Get directory of build script
+#Get directory of build script change to it in case the
+#script was not run from its directory
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd ${DIR}
+
+#Remove old temp directory if it exists and make new
+rm -rf temp
+mkdir temp
+
+#Make layer folder
+mkdir lambda/layers
 
 #Package nodejs modules
-cd ${DIR}/lambda/modules
-npm pack ./database-driver
-
+cd temp
+npm pack ../lambda/modules/database-driver
 
 #Install nodejs modules in layers and package in zip
-
-cd ${DIR}/lambda/layers/database-driver-layer/nodejs
-npm install ${DIR}/lambda/modules/database-driver-1.0.0.tgz
+mkdir nodejs
+cd nodejs
+npm install ../database-driver-1.0.0.tgz
 cd ..
-zip -r ../database-driver-layer.zip nodejs
+zip -r ../lambda/layers/database-driver-layer.zip nodejs
+rm -rf nodejs
+
+#Change back root directory and remove temp folder
+cd ${DIR}
+rm -rf temp
--- lambda/lambda.tf
+++ lambda/lambda.tf
@@ -1,4 +1,3 @@
-
 resource "aws_lambda_layer_version" "database_driver_layer" {
   filename   = "./lambda/layers/database-driver-layer.zip"
   layer_name = "databaseDriverLayer"
