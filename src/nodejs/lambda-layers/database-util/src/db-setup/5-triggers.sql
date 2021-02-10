2376e45af498fc470b2b8d29f9267071dbb0a513
Brian Ellingson <brian.ellingson@uah.edu>
2021-02-10 12:32:23 -0600

    Remove generated sql files

--- src/nodejs/lambda-layers/database-util/src/db-setup/5-triggers.sql
+++
@@ -1,8 +0,0 @@
-DROP TRIGGER IF EXISTS insert_submission ON submission;
-DROP TRIGGER IF EXISTS update_submission_status ON submission_status;
-
-CREATE TRIGGER insert_submission AFTER INSERT ON submission
-FOR EACH ROW EXECUTE PROCEDURE init_submission();
-
-CREATE TRIGGER update_submission_status AFTER UPDATE ON submission_status
-FOR EACH ROW EXECUTE PROCEDURE check_workflow_state();
--- src/nodejs/lambda-layers/database-util/src/db-setup/7-aggregates.sql
+++
@@ -1,7 +0,0 @@
-DROP AGGREGATE IF EXISTS jsonb_merge_agg(jsonb);
-
-CREATE AGGREGATE jsonb_merge_agg(jsonb) (
-  SFUNC = 'jsonb_concat',
-  STYPE = JSONB,
-  INITCOND = '{}'
-);
