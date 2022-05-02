6a68d6dedc22fe7e9483043e9e8432d9918dc2f5
ecampos333 <ebc0007@uah.edu>
2022-05-02 12:51:23 -0500

    Update workflow query so init and close are captured.

--- src/nodejs/lambda-layers/database-util/src/query/workflow.js
+++ src/nodejs/lambda-layers/database-util/src/query/workflow.js
@@ -6,23 +6,28 @@ const findAll = () => `
   FROM workflow
   LEFT JOIN (
     SELECT
-      back_edge.workflow_id,
-      JSONB_OBJECT_AGG(step.step_name,
-      JSONB_STRIP_NULLS(JSONB_BUILD_OBJECT(
-      'type', step.type,
-      'action_id', step.action_id,
-      'form_id', step.form_id,
-      'service_id', step.service_id,
-      'next_step_name', step_edge.next_step_name,
-      'prev_step_name', back_edge.prev_step_name
-    ))) steps
+    step_edge_details.workflow_id,
+      JSONB_OBJECT_AGG( step.step_name,
+        JSONB_STRIP_NULLS(JSONB_BUILD_OBJECT(
+          'type', step.type,
+          'action_id', step.action_id,
+          'form_id', step.form_id,
+          'service_id', step.service_id,
+          'next_step_name', step_edge_details.next_step_name,
+          'prev_step_name', step_edge_details.prev_step_name
+      ))) steps
     FROM step
-    NATURAL LEFT JOIN step_edge
     NATURAL LEFT JOIN (
-      SELECT step_edge.workflow_id, step_edge.next_step_name step_name, step_edge.step_name prev_step_name
-      FROM step_edge) back_edge
-    GROUP BY back_edge.workflow_id) step_json
-    ON step_json.workflow_id = workflow.id`;
+      SELECT  *
+      FROM step_edge
+      NATURAL FULL OUTER JOIN (
+        SELECT workflow_id, next_step_name step_name, step_name prev_step_name
+        FROM step_edge
+      ) back_edge
+    ) step_edge_details
+    group by step_edge_details.workflow_id
+  ) step_json ON step_json.workflow_id = workflow.id
+`;
 const findById = () => `${findAll()} WHERE workflow.id = {{id}}`;
 
 module.exports.findAll = findAll;
