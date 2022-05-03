87d4ce83f1b979e2fe8ae82e0c23eb04b25f64bb
ecampos333 <ebc0007@uah.edu>
2022-05-03 16:34:38 -0500

    resolve linting errors.

--- src/nodejs/lambda-handlers/workflow-consumer.js
+++ src/nodejs/lambda-handlers/workflow-consumer.js
@@ -22,7 +22,8 @@ async function actionMethod(status) {
     step_name: status.step.name,
     data: status.step.data
   };
-  Object.keys(eventMessage).forEach(key => eventMessage[key] === undefined && delete eventMessage[key]);
+  Object.keys(eventMessage).forEach((key) => (
+    eventMessage[key] === undefined && delete eventMessage[key]));
   await msg.sendEvent(eventMessage);
 }
 
