1489a3151604fc7a632bab61fe4ca5ddef869d5b
ecampos333 <ebc0007@uah.edu>
2022-06-13 14:26:26 -0500

    Update page content schema to array of objects.

--- src/nodejs/lambda-layers/schema-util/src/models/page.js
+++ src/nodejs/lambda-layers/schema-util/src/models/page.js
@@ -5,28 +5,31 @@ module.exports.model = () => ({
   properties: {
     page_key: {type: "string"},
     content: {
-      type: "object",
-      required: ["id", "heading"],
-      properties: {
-        id: {type: "number"},
-        heading: {type: "string"},
-        paragraphs: {type: "array", items: {type: "string"}},
-        list: {type: "array", items: {type: "string"}},
-        table: {
-          type: "object",
-          properties: {
-            rows: {
-              type: "array",
-              items: {
-                type: "object",
-                properties: {
-                  number: {type: "number"},
-                  columns: {type: "array", items: {type: "string"}}
+      type: "array",
+      items: {
+        type: "object",
+        required: ["id", "heading"],
+        properties: {
+          id: {type: "number"},
+          heading: {type: "string"},
+          paragraphs: {type: "array", items: {type: "string"}},
+          list: {type: "array", items: {type: "string"}},
+          table: {
+            type: "object",
+            properties: {
+              rows: {
+                type: "array",
+                items: {
+                  type: "object",
+                  properties: {
+                    number: {type: "number"},
+                    columns: {type: "array", items: {type: "string"}}
+                  }
                 }
-              }
-            },
-            header: {type: "array", items: {type: "string"}},
-            caption: {type: "string"}
+              },
+              header: {type: "array", items: {type: "string"}},
+              caption: {type: "string"}
+            }
           }
         }
       }
