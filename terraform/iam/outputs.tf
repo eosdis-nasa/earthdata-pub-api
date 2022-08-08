cae54081a3386da149a2185b519f5068da61ab56
ecampos333 <ebc0007@uah.edu>
2022-08-08 01:07:45 -0500

    Create lambda iam role to access rds.

--- terraform/iam/outputs.tf
+++ terraform/iam/outputs.tf
@@ -5,3 +5,7 @@ output "edpub_lambda_role_arn" {
 output "edpub_apigateway_s3_role_arn" {
   value = aws_iam_role.edpub_apigateway_s3_role.arn
 }
+
+output "edpub_rds_backup_lambda_role_arn" {
+  value = aws_iam_role.edpub_rds_backup_lambda_role
+}
