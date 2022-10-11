8ac5f49ed84df24cfc4823fd1e3ab099b6fbe0e3
Kimberly Broughton <broughtonkk@ornl.gov>
2022-10-11 16:26:08 -0400

    remodeled page for overview app updates

--- src/nodejs/lambda-layers/schema-util/src/models/page.js
+++ src/nodejs/lambda-layers/schema-util/src/models/page.js
@@ -19,13 +19,23 @@ module.exports.model = (path) => ({
             properties: {
               text: { type: 'string' },
               list: {type: "array", items: {type: "string"}},
-              note: {type: "string"},
               image: {type: "string"},
               image_alt_text: {type: "string"},
               icon: {type: "string"},
+              icon_text: {type: "string"},
               heading: { type: 'string' },
-              caption: {type: "string"},
               button: { type: 'string' },
+              three_columns: {
+                type: "array", items: {
+                  type: "object",
+                  required: ["text"],
+                  properties: {
+                    text: { type: 'string' },
+                    image: { type: 'string' },
+                    image_alt_text: { type: 'string' }
+                  }
+                }
+              },
               box_list: {
                 type: "array", items: {
                   type: "object",
@@ -34,20 +44,8 @@ module.exports.model = (path) => ({
                     heading: { type: 'string' },
                     text: { type: 'string' },
                     icon: { type: 'string' },
-                    box_link: { type: 'string' }
-                  }
-                }
-              },
-              info_bubble: {
-                type: "array", items: {
-                  type: "object",
-                  required: ["text","linked_cls"],
-                  properties: {
-                    linked_cls:  { type: 'string' },
-                    heading: { type: 'string' },
-                    text: { type: 'string' },
-                    icon: { type: 'string' },
-                    link: { type: 'string' }
+                    box_link: { type: 'string' },
+                    link_name: { type: 'string' }
                   }
                 }
               },
@@ -61,11 +59,24 @@ module.exports.model = (path) => ({
                     text: { type: 'string' },
                     icon: { type: 'string' },
                     icon_text: { type: 'string' },
+                    link_name: { type: 'string' },
+                    box_link: { type: 'string' },
                     button: { type: 'string' },
                     image: {type: "string"},
                     image_alt_text: {type: "string"},
-                    tip: {type: "string"},
-                    accordian: {type: "string"}
+                    accordian_header: {type: "string"},
+                    accordian_body: {type: "string"}
+                  }
+                }
+              },
+              grouped_note: {
+                type: "array", items: {
+                  type: "object",
+                  required: ["icon","icon_text"],
+                  properties: {
+                    heading: { type: 'string' },
+                    icon: { type: 'string' },
+                    icon_text: { type: 'string' }
                   }
                 }
               },
@@ -89,40 +100,7 @@ module.exports.model = (path) => ({
             }
           }
         },
-        list: {type: "array", items: {type: "string"}},
-        image: {type: "string"},
-        image_alt_text: {type: "string"},
-        icon: {type: "string"},
-        table: {
-          type: "object",
-          properties: {
-            rows: {
-              type: "array",
-              items: {
-                type: "object",
-                properties: {
-                  number: {type: "number"},
-                  columns: {type: "array", items: {type: "string"}}
-                }
-              }
-            },
-            heading: {type: "array", items: {type: "string"}},
-            caption: {type: "string"}
-          }
-        },
-        box_list: {
-          type: "array", items: {
-            type: "object",
-            required: ["text"],
-            properties: {
-              heading: { type: 'string' },
-              text: { type: 'string' },
-              icon: { type: 'string' },
-              box_link: { type: 'string' }
-            }
-          }
-        }
-      }
+      }  
     }
   }
 });
