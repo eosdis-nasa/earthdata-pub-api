391374c1464ca3feb7255fe90e02ce4f020488a8
Davis Gaunt <bdg0015@uah.edu>
2022-12-02 13:20:07 -0600

    added support for creating a workflow as well as creating and editing steps.

--- src/nodejs/lambda-handlers/workflow.js
+++ src/nodejs/lambda-handlers/workflow.js
@@ -7,24 +7,83 @@
 
 const db = require('database-util');
 
+function validateWorkflow(steps) {
+  let i = 0;
+  let activeStep = steps.init;
+  while (activeStep.next_step_name !== 'close') {
+    if (
+      (i > 100)
+      || (!activeStep.next_step_name)
+      // eslint-disable-next-line
+      || (!(activeStep = steps[activeStep.next_step_name]))
+    ) { return false; }
+    i += 1;
+  }
+  return true;
+}
+
+async function createStep(step, stepName) {
+  const type = step.type ? step.type : null;
+  const actionId = step.actionId ? step.actionId : null;
+  const formId = step.formId ? step.formId : null;
+  const serviceId = step.service_id ? step.service_id : null;
+  const data = step.prev_step ? step.prev_step : null;
+  await db.workflow.createStep({
+    step_name: stepName,
+    type,
+    action_id: actionId,
+    form_id: formId,
+    service_id: serviceId,
+    data
+  });
+}
+// eslint-disable-next-line
+async function addSteps(steps, workflow_id) {
+  let activeStepName = 'init';
+  let activeStep = steps[activeStepName];
+  while (activeStep.next_step_name) {
+    const nextStepName = activeStep.next_step_name;
+    if(nextStepName !== 'close'){await createStep(steps[nextStepName], nextStepName);}
+    await db.workflow.addStep({
+      workflow_id,
+      step_name: activeStepName,
+      next_step_name: nextStepName
+    });
+    activeStep = steps[activeStepName = activeStep.next_step_name];
+  }
+}
+
+async function createWorkflowMethod(params, user) {
+  const {
+    // eslint-disable-next-line
+    short_name, version, long_name, description, steps
+  } = params;
+
+  if (user.user_roles.some((role) => approvedUserRoles.includes(role.short_name))) {
+    if (!validateWorkflow(steps)) { return ({ status: 'Invalid Workflow' }); }
+    const { id } = await db.workflow.initialize({
+      short_name, version, long_name, description
+    });
+    await addSteps(steps, id);
+    await db.workflow.addClose({ workflow_id: id });
+
+    return (db.workflow.findById({ id }));
+  }
+
+  return ({ status: 'Invalid Permissions' });
+}
+
 async function editWorkflowMethod(params, user) {
   const {
     id, version, description, steps
   } = params;
-  let activeStepName = 'init';
-  let activeStep = steps[activeStepName];
+
   const approvedUserRoles = ['admin'];
   if (user.user_roles.some((role) => approvedUserRoles.includes(role.short_name))) {
+    if (!validateWorkflow(steps)) { return ({ status: 'Invalid Workflow' }); }
+
     await db.workflow.clearSteps({ id });
-    while (activeStep.next_step_name) {
-      const nextStepName = activeStep.next_step_name;
-      await db.workflow.addStep({
-        workflow_id: id,
-        step_name: activeStepName,
-        next_step_name: nextStepName
-      });
-      activeStep = steps[activeStepName = activeStep.next_step_name];
-    }
+    await addSteps(steps, id);
     await db.workflow.addClose({ workflow_id: id });
     return (db.workflow.updateWorkflowMetaData({
       version,
@@ -36,7 +95,8 @@ async function editWorkflowMethod(params, user) {
 }
 
 const operations = {
-  editWorkflow: editWorkflowMethod
+  editWorkflow: editWorkflowMethod,
+  createWorkflow: createWorkflowMethod
 };
 
 async function handler(event) {
