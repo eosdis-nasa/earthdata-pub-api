d62367b9933ca37a17ac1b24a608879fdecac5a3
Davis Gaunt <bdg0015@uah.edu>
2022-12-21 13:46:04 -0600

    rewrote crate step to use teh sql-builder

--- src/nodejs/lambda-handlers/workflow.js
+++ src/nodejs/lambda-handlers/workflow.js
@@ -26,19 +26,15 @@ function validateWorkflow(steps) {
 }
 
 async function createStep(step, stepName) {
-  const type = step.type ? step.type : null;
-  const actionId = step.actionId ? step.actionId : null;
-  const formId = step.formId ? step.formId : null;
-  const serviceId = step.service_id ? step.service_id : null;
-  const data = step.prev_step ? step.prev_step : null;
-  await db.workflow.createStep({
+  const output = await db.workflow.createStep({
     step_name: stepName,
-    type,
-    action_id: actionId,
-    form_id: formId,
-    service_id: serviceId,
-    data
+    type: step.type,
+    action_id: step.action_id,
+    form_id: step.form_id,
+    service_id: step.service_id,
+    data: step.prev_step
   });
+  console.log(output)
 }
 
 async function addSteps(steps, workflowId) {
