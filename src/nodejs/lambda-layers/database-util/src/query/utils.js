e06ab5096ca3e2e1f75b902b5b759c992defbd96
Davis Gaunt <bdg0015@uah.edu>
2023-04-10 09:32:04 -0500

    fixes issue with ' and " breaking inputs.  does sanitation to protect against SQL injection attacks.

--- src/nodejs/lambda-layers/database-util/src/query/utils.js
+++ src/nodejs/lambda-layers/database-util/src/query/utils.js
@@ -4,6 +4,8 @@ const pubCleanJSON = (dirtyJSON) => {
 
     let cleanJSON = dirtyJSON.replace(/'/g, "\\'");
     cleanJSON = cleanJSON.replace(/\\"/g, '\\\\"');
+    cleanJSON = cleanJSON.replace(/\\n/g, '\\\\n');
+    cleanJSON = cleanJSON.replace(/\\t/g, '\\\\t');
     return cleanJSON;
 }
 
