4e543e83daa0c1bd3b0875418d79db382fab7b66
Davis Gaunt <bdg0015@uah.edu>
2023-04-27 11:00:35 -0500

    adding comments to s3 TF deployment

--- terraform/s3/main.tf
+++ terraform/s3/main.tf
@@ -1,4 +1,8 @@
+// Most EDPub buckets are currently manually created to avoid any accidental deletion. 
+//The upload bucket is an exception in order to ensure cors configurations are maintained and deployed correctly.
+//Upload bucket
 resource "aws_s3_bucket" "earthdatapub-upload" {
+  // Data upload should only be enabled in SIT until data upload solution finalized
   count = contains(["sit"], var.environment) ? 1 : 0
   bucket = "earthdatapub-upload-${var.environment}"
   lifecycle {
@@ -6,7 +10,7 @@ resource "aws_s3_bucket" "earthdatapub-upload" {
   }
 }
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "upload-encription" {
+resource "aws_s3_bucket_server_side_encryption_configuration" "upload-encryption" {
   count = contains(["sit"], var.environment) ? 1 : 0
   bucket = aws_s3_bucket.earthdatapub-upload.id
   rule {
