7e5de58044db18691133f0ede7dbbbbf6a8e5326
Kimberly Broughton <broughtonkk@ornl.gov>
2023-09-15 09:20:34 -0400

    added function for multiple email templates; added direct message

--- src/nodejs/lambda-layers/message-util/src/create-email.js
+++ src/nodejs/lambda-layers/message-util/src/create-email.js
@@ -1,4 +1,5 @@
 const db = require('database-util');
+const sourceEmail = process.env.SOURCE_EMAIL;
 
 const htmlSnippets = {
     default_style: () => ({
@@ -14,110 +15,125 @@ const htmlSnippets = {
         text: '<b>Step Change</b>'
     }),
     greeting: (params) => ({
-        text: `<h1>Hello ${params.user.name},</h1><br>`
+        text: `<h1>Hello ${params.user.name},</h1><br><br>`
+    }),
+    request_body: (params) => ({
+        text: `<h2>Request:</h2><p>${params.eventMessage.submission_name} (${params.eventMessage.submission_id})<br><a style="text-align: left;" href="https://pub.earthdata.nasa.gov/dashboard/requests/id/${params.eventMessage.submission_id}" aria-label="View the request">Go to request</a></p>`
     }),
     step_change_body1: (params) => ({
         text: `<p>The following request has changed step in the ${params.eventMessage.workflow_name} workflow.</p>`
     }),
     step_change_body2: (params) => ({
-        text: `<h2>Request:</h2><p>${params.eventMessage.submission_name} (${params.eventMessage.submission_id})<br><a style="text-align: left;" href="https://pub.earthdata.nasa.gov/dashboard/requests/id/${params.eventMessage.submission_id}" aria-label="View the request">Go to request</a></p>`
-    }),
-    step_change_body3: (params) => ({
         text: `<h2>New Step:</h2><p>${params.eventMessage.step_name}</p>`
     }),
-    step_change_body4: (params) => ({
+    step_change_body3: (params) => ({
         text: `<h2>Comments:</h2><p>${params.eventMessage.conversation_last_message}</p><br><br>`
     }),
     dashboard_link: () => ({
         text: '<p><a style="text-align: left;" href="https://pub.earthdata.nasa.gov/dashboard" aria-label="Visit Earthdata Pub Dashboard">https://pub.earthdata.nasa.gov/dashboard</a></p>'
+    }),
+    step_change_as_text: (params) => ({
+        text: `Hello ${params.user.name},\n\nThe following request has changed step in the ${params.eventMessage.workflow_name} workflow.\n\nRequest:\n${params.eventMessage.submission_name} (${params.eventMessage.submission_id})\n\nNew Step:\n${params.eventMessage.step_name}\n\nComments:\n${params.eventMessage.conversation_last_message}`
+    }),
+    direct_message: () => ({
+        text: '<p>You have received a direct message on the Earthdata Pub Dashboard.</p>'
+    }),
+    event_type_for_direct_message: () => ({
+        text: '<b>Direct Message Sent</b>'
+    }),
+    direct_message_body1: (params) => ({
+        text: `<h2>Comments:</h2><p>${params.eventMessage.conversation_last_message}</p><br><br>`
+    }),
+    direct_message_as_text: (params) => ({
+        text: `Hello ${params.user.name},\n\nYou have received a direct message on the Earthdata Pub Dashboard.\n\nRequest:\n${params.eventMessage.submission_name} (${params.eventMessage.submission_id})\n\nComments:\n${params.eventMessage.conversation_last_message}`
     })
 };
 
-/* const createEmailBody = async (params) => {
-    const style = htmlSnippets.default_style.text;
-    const logo = htmlSnippets.logo.text;
-    const title = htmlSnippets.title.text;
-    const stepChange = htmlSnippets.step_change.text;
-    const greeting = htmlSnippets.greeting.text(params);
-    const stepChangeBody1 = htmlSnippets.step_change_body1.text;
-    const stepChangeBody2 = htmlSnippets.step_change_body2.text;
-    const stepChangeBody3 = htmlSnippets.step_change_body3.text;
-    const stepChangeBody4 = htmlSnippets.step_change_body4.text;
-    const dashboardLink = htmlSnippets.dashboard_link.text;
-    const HTML = `
-        <html>
-            <body>
-                <style>${style || null}</style>
-                <table border="0" cellpadding="10" cellspacing="0" style="width:100%">
-                  <tr style="width:100%;background:#f8f8f8">
-                      <td>
-                        <table>
-                          <tr>
-                            <td width="60">${logo || null}</td>
-                            <td>${title || null}</td>
-                          </tr>
-                        </table>
-                      </td>
-                      <td align="right">${stepChange || null}</td>
-                      <td></td>
-                  </tr>
-                  <tr>
-                    <td colspan="2" style="padding:20px;">
-                      ${greeting || null}
-                      <br>
-                      ${stepChangeBody1 || null}
-                      ${stepChangeBody2 || null}
-                      ${stepChangeBody3 || null}
-                      ${stepChangeBody4 || null}
-                      ${dashboardLink || null}
-                    </td>
-                  </tr>
-                </table>
-            </body>
-        </html> 
-    `;
-    return HTML;
-}; */
+const createEmailHtml = async (params) => {
+    const style = htmlSnippets.default_style().text;
+    const logo = htmlSnippets.logo().text;
+    const title = htmlSnippets.title().text;
+    const greeting = htmlSnippets.greeting(params).text;
+    const requestBody = htmlSnippets.request_body(params).text;
+    const dashboardLink = htmlSnippets.dashboard_link().text;
 
+    let stepChange, stepChangeBody1, stepChangeBody2, stepChangeBody3, stepChangeAsText;
+    let eventTypeForDirectMessage, directMessage, directMessageBody1, directMessageAsText;
 
-const createEmailBody = async (params) => {
-    console.log('getHTML', params);
-    // [{ email: 'broughtonkk@ornl.gov', name: 'Kimberly Broughton' }]
+    if (params.eventMessage.event_type.match(/direct_message/gi)) {
+        eventTypeForDirectMessage = htmlSnippets.event_type_for_direct_message().text;
+        directMessage = htmlSnippets.direct_message().text;
+        directMessageBody1 = htmlSnippets.direct_message_body1(params).text;
+        directMessageAsText = htmlSnippets.direct_message_as_text(params).text;
+    } else {
+        stepChange = htmlSnippets.step_change().text;
+        stepChangeBody1 = htmlSnippets.step_change_body1(params).text;
+        stepChangeBody2 = htmlSnippets.step_change_body2(params).text;
+        stepChangeBody3 = htmlSnippets.step_change_body3(params).text;
+        stepChangeAsText = htmlSnippets.step_change_as_text(params).text;
+    }
     const HTML = `
-        <html>
-            <body>
-                <style></style>
-                <table border="0" cellpadding="10" cellspacing="0" style="width:100%">
-                    <tr style="width:100%;background:#f8f8f8">
-                        <td><table><tr>
-                            <td width="60"><img src="https://pub.earthdata.nasa.gov/dashboard/images/app/src/assets/images/nasa-logo.78fcba4d9325e8ac5a2e15699d035ee0.svg"></td>
-                            <td><h4>Earthdata Pub</h4></td>
-                        </tr></table></td>
-                        <td align="right"><b>Step Change</b></td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td colspan="2" style="padding:20px;">
-                            <h1>Hello ${user.name},</h1><br>
-                            <br>
-                            <p>The following request has changed step in the ${eventMessage.workflow_name} workflow.</p>
-                            <h2>Request:</h2>
-                            <p>${eventMessage.submission_name} (${eventMessage.submission_id})<br>
-                            <a style="text-align: left;" href="https://pub.earthdata.nasa.gov/dashboard/requests/id/${eventMessage.submission_id}" aria-label="View the request">Go to request</a></p>
-                            <h2>New Step:</h2>
-                            <p>${eventMessage.step_name}</p>
-                            <h2>Comments:</h2> 
-                            <p>${eventMessage.conversation_last_message}</p>
-                            <br><br>
-                            <p><a style="text-align: left;" href="https://pub.earthdata.nasa.gov/dashboard" aria-label="Visit Earthdata Pub Dashboard">https://pub.earthdata.nasa.gov/dashboard</a></p>
-                        </td>
-                    </tr>
-                </table>
-            </body>
-        </html> 
-    `
-    return HTML
+    <html>
+        <body>
+            <style>${style || ''}</style>
+            <table border="0" cellpadding="10" cellspacing="0" style="width:100%">
+              <tr style="width:100%;background:#f8f8f8">
+                  <td>
+                    <table>
+                      <tr>
+                        <td width="60">${logo || ''}</td>
+                        <td>${title || ''}</td>
+                      </tr>
+                    </table>
+                  </td>
+                  <td align="right">${stepChange || eventTypeForDirectMessage || ''}</td>
+                  <td></td>
+              </tr>
+              <tr>
+                <td colspan="2" style="padding:20px;">
+                  ${greeting || ''}
+                  ${stepChangeBody1 || directMessage || ''}
+                  ${requestBody || ''}
+                  ${stepChangeBody2 || directMessageBody1 || ''}
+                  ${stepChangeBody3 || ''}
+                  ${dashboardLink || ''}
+                </td>
+              </tr>
+            </table>
+        </body>
+    </html> 
+  `;
+    console.log(HTML)
+    return [stepChangeAsText || directMessageAsText, HTML];
 };
 
-module.exports.createEmailBody = createEmailBody;
-module.exports.htmlSnippets = htmlSnippets;
+const createEmailPayload = async (params) => {
+    const users = params.users;
+    users.forEach(async (user) => {
+        const eventMessage = params.eventMessage;
+        const bodyArray = createEmailHtml({ user, eventMessage });
+        const emailPayload = {
+            Source: process.env.SOURCE_EMAIL,
+            Destination: {
+                ToAddresses: [user.email]
+            },
+            Message: {
+                Subject: {
+                    Data: 'EDPUB Notification'
+                },
+                Body: {
+                    Text: {
+                        Data: bodyArray[0]
+                    },
+                    Html: {
+                        Data: bodyArray[1]
+                    }
+                }
+            }
+        };
+        console.log('emailPayload', emailPayload);
+        return emailPayload;
+    });
+};
+
+module.exports.createEmailHtml = createEmailHtml;
