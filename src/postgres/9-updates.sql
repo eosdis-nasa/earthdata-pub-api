d91e67ebf5c5e0cfd1370530b8d0afa07cd51ac9
cey <dunivanck@ornl.gov>
2023-12-21 16:47:38 -0500

    Fixing update statement to update the correct question

--- src/postgres/9-updates.sql
+++ src/postgres/9-updates.sql
@@ -53,5 +53,5 @@ RAISE;
 -- 12/21/2023 Added Additional Acknowledgement Extended Question for NSIDC
 INSERT INTO question(id, short_name, version, long_name, text, help, required, daac_ids) VALUES ('d3c4f81e-1954-4b6f-9edf-90f240f525a8', 'acknowledgement', 1, 'Additional Acknowledgments', 'If there are people or groups who are not identified in the Data Citation but whose contributions to the data product should be acknowledged, please name them here.', 'The DAAC will work with you to add this information to an Acknowledgements section of the data product user guide.', False, '{"aec3724f-b30b-4b3f-9b9a-e0907d9d14b3"}');
 INSERT INTO section_question VALUES ('768a6b51-4864-458c-b20d-fb8b4c7dc606', 'd3c4f81e-1954-4b6f-9edf-90f240f525a8', 4, '[]', '[]');
-UPDATE section_question set list_order=5 where question_id = 'd3c4f81e-1954-4b6f-9edf-90f240f525a8' and section_id='768a6b51-4864-458c-b20d-fb8b4c7dc606';
+UPDATE section_question set list_order=5 where question_id = '2dd6c8b1-22a8-4866-91c3-da9b4ce849dc' and section_id='768a6b51-4864-458c-b20d-fb8b4c7dc606';
 INSERT INTO input VALUES ('d3c4f81e-1954-4b6f-9edf-90f240f525a8', 'acknowledgement', 0, 'Acknowledgement', 'text', '{}', '{}', '[]', '[]');
