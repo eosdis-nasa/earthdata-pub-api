14e064840c2f2f1f31a29f315a8ca04b9340fd9f
Davis <bdg0015@uah.edu>
2024-01-26 09:55:33 -0600

    update check on sourse file hash not zip

--- terraform/lambda/main.tf
+++ terraform/lambda/main.tf
@@ -1046,7 +1046,7 @@ resource "aws_lambda_invocation" "rds_update" {
   function_name         = aws_lambda_function.rds_update.function_name
   
   triggers  = {
-    lambda_function_last_modified    = aws_lambda_function.rds_update.last_modified
+    redeployment    =  filesha256("../src/nodejs/lambda-handlers/rds-update")
   }
 
   input                 = jsonencode({})
