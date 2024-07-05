bd57be2d3b3a4fb9e9d24c0f1bc4050f5e4bc51b
camposeddie <eddie.campos@uah.edu>
2024-07-05 11:30:44 -0500

    Add new template filter

--- src/nodejs/lambda-layers/database-util/src/query/note.js
+++ src/nodejs/lambda-layers/database-util/src/query/note.js
@@ -330,7 +330,7 @@ WHERE conversation_id = {{conversation_id}} AND edpuser_id = {{user_id}}
 `;
 
 const getEmails = (params) => sql.select({
-  fields: ['email', 'name'],
+  fields: ['id', 'email', 'name'],
   from: {
     base: 'edpuser',
     joins: [{
--- src/nodejs/lambda-layers/message-util/src/create-email.js
+++ src/nodejs/lambda-layers/message-util/src/create-email.js
@@ -9,7 +9,7 @@ const createEmailHtml = async (params) => {
   if (params.eventMessage.event_type.match(/direct_message/gi)) {
     return getDMTemplate(params, envUrl);
   } if (params.eventMessage.event_type.match(/request_initialized/gi)) {
-    return getNewSubmissionTemplate(params, envUrl);
+    return params.user.initiator ?  getNewSubmissionTemplate(params, envUrl) : getNewSubmissionDAACTemplate(params, envUrl);
   }
   return getDefaultStepPromotion(params, envUrl);
 };
