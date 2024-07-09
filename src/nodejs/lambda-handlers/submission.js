62ce8ebf950171974ada63d042ef88de2309a6bb
Dunivan Stahl, Chelsey K <dunivanck@ornl.gov>
2024-07-09 15:36:05 -0400

    logging withdrawn submissions

--- src/nodejs/lambda-handlers/submission.js
+++ src/nodejs/lambda-handlers/submission.js
@@ -216,8 +216,23 @@ async function unlockMethod(event, user) {
 
 async function withdrawMethod(event, user) {
   const { id } = event;
+  const status = await db.submission.getState({ id });
+  const submissionMetrics = await db.metrics.getSubmissions({
+    submissionId: id
+  });
+  
+  const eventMessage = {
+    event_type: 'workflow_completed',
+    submission_id: id,
+    conversation_id: status.conversation_id,
+    workflow_id: status.workflow_id,
+    step_name: 'withdrawn',
+    time_to_publish: Math.round(submissionMetrics[0].time_to_publish)
+  };
+
   const approvedUserPrivileges = ['REQUEST_ADMINREAD', 'ADMIN', 'REQUEST_DAACREAD'];
   if (approvedUserPrivileges.some((privilege) => user.user_privileges.includes(privilege))) {
+    await msg.sendEvent(eventMessage);
     return db.submission.withdrawSubmission({ id });
   }
   return db.submission.findById({ id });
