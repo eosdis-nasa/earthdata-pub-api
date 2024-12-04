8cef4d799022d45b82823a081a29bc406f37b3ea
acharya-deepak <deepak.acharya@uah.edu>
2024-12-03 18:33:41 -0600

    another try logo

--- src/nodejs/lambda-layers/message-util/src/templates/direct-message.js
+++ src/nodejs/lambda-layers/message-util/src/templates/direct-message.js
@@ -1,4 +1,25 @@
-const getDMTemplate = (params, envUrl) => {
+import fetch from 'node-fetch';
+
+const svgUrl = 'https://pub.earthdata.nasa.gov/dashboard/images/app/src/assets/images/nasa-logo.78fcba4d9325e8ac5a2e15699d035ee0.svg';
+
+const toBase64 = async (response) => {
+  const buffer = await response.buffer();
+  return buffer.toString('base64');
+};
+
+const fetchSvgAndConvertToBase64 = async (url) => {
+  try {
+    const response = await fetch(url);
+    const base64EncodedData = await toBase64(response);
+    return `data:image/svg+xml;base64,${base64EncodedData}`;
+  } catch (error) {
+    console.error('Error fetching or converting SVG:', error);
+    return null; // return null or a default image URL in case of an error
+  }
+};
+
+const getDMTemplate = async (params, envUrl) => {
+  const svgDataUri = await fetchSvgAndConvertToBase64(svgUrl);
   const text = `Hello ${params.user.name},\n\nYou have received a direct message on the Earthdata Pub Dashboard.\n\nMessage:\n${params.eventMessage.conversation_last_message}`;
   const html = `
     <html>
@@ -9,7 +30,7 @@ const getDMTemplate = (params, envUrl) => {
                  <td>
                    <table>
                      <tr>
-                       <td width="60"><img src = "https://pub.earthdata.nasa.gov/dashboard/images/app/src/assets/images/nasa-logo.78fcba4d9325e8ac5a2e15699d035ee0.svg"></td>
+                       <td width="60"><img src = "${svgDataUri}"></td>
                        <td><h4>Earthdata Pub</h4></td>
                      </tr>
                    </table>
