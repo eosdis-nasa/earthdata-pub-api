9316d2041addfa83d806940326473e9c84c4e6c7
acharya-deepak <deepak.acharya@uah.edu>
2024-12-05 21:39:06 -0600

    try again

--- src/nodejs/lambda-layers/message-util/src/create-email.js
+++ src/nodejs/lambda-layers/message-util/src/create-email.js
@@ -1,25 +1,8 @@
-const https = require('https');
 const { getDefaultStepPromotion } = require('./templates/default-step-promotion');
 const { getNewSubmissionTemplate } = require('./templates/new-submission');
 const { getNewSubmissionDAACTemplate } = require('./templates/new-submission-daac');
 const { getDMTemplate } = require('./templates/direct-message');
 const { getReviewerAddedTemplate } = require('./templates/review-template');
-
-const fetchSvgContent = (url) => new Promise((resolve, reject) => {
-  https.get(url, (res) => {
-    if (res.statusCode !== 200) {
-      reject(new Error(`Request failed with status code ${res.statusCode}`));
-      return; // Necessary for consistent behavior
-    }
-
-    let data = '';
-    res.on('data', (chunk) => {
-      data += chunk;
-    });
-
-    res.on('end', () => resolve(data));
-  }).on('error', reject);
-});
 
 const envUrl = process.env.ROOT_URL;
 
@@ -39,10 +22,8 @@ const createEmailHtml = async (params) => {
   }
 
   if (params.eventMessage.event_type === 'review_required') {
-    const svgUrl = 'https://pub.earthdata.nasa.gov/dashboard/images/app/src/assets/images/nasa-logo.78fcba4d9325e8ac5a2e15699d035ee0.svg';
     try {
-      const svgContent = await fetchSvgContent(svgUrl);
-      return getReviewerAddedTemplate(params, envUrl, svgContent);
+      return getReviewerAddedTemplate(params, envUrl);
     } catch (error) {
       console.error('Error fetching SVG:', error.message);
       return getReviewerAddedTemplate(params, envUrl);
--- src/nodejs/lambda-layers/message-util/src/templates/review-template.js
+++ src/nodejs/lambda-layers/message-util/src/templates/review-template.js
@@ -1,6 +1,34 @@
-const getReviewerAddedTemplate = async (params, envUrl, xx) => {
+const https = require('https');
+
+const getReviewerAddedTemplate = async (params, envUrl) => {
+  const fetchImage = async (url) => new Promise((resolve, reject) => {
+    https.get(url, (res) => {
+      let data = '';
+      res.on('data', (chunk) => {
+        data += chunk;
+      });
+      res.on('end', () => {
+        if (res.statusCode === 200) {
+          resolve(data);
+        } else {
+          reject(new Error(`Failed to fetch image. Status: ${res.statusCode}`));
+        }
+      });
+    }).on('error', (err) => {
+      reject(new Error(`Error: ${err.message}`));
+    });
+  });
+
+  // Fetch the <img> tag or fallback if an error occurs
+  let imgTag = '';
+  try {
+    imgTag = await fetchImage('https://pub.sit.earthdata.nasa.gov/image');
+  } catch (error) {
+    console.error(error);
+    imgTag = '<img src="" alt="Placeholder Image" style="display:block"/>'; // Fallback image
+  }
+
   // Fallback Base64 image content (example provided here)
-  const base64Logo = 'https://earthdatapub-dashboard-sit.s3.us-west-2.amazonaws.com/images/app/src/assets/images/nasa_test.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=ASIAQXSOCK3LERGUXYXW%2F20241206%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20241206T025531Z&X-Amz-Expires=216000&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEGsaCXVzLXdlc3QtMiJHMEUCIAmOtOdT3Wk8p2Jy5ABKzK31E5noupcXudGcd2K8jbuVAiEAzgd9K64pZa%2Fy6Tp9vQTR6p5qnJmP9xb81mP7fi0o7jcq6AIIJBADGgwwNTA2Mjk1OTY4ODYiDJrMHHRd9TpCDdmw3CrFAlGD808LGe4rXyqPReBOtsU8rXspiBJceFaXGzjy6OJU4fcP8yQtuFq0hhWfvED6D%2FrFNeEI9Z%2F3v6YdXNDjPCCo2eviJmOMGx%2Fnm5Qehkn%2BOcNFHtwx3jwf83jv%2BMjmS6bht3BrgjGWS6TkyB0bF3d4oFwyxJCz67zWYI%2BJRVlsrgTn%2BVC39vJlhoXhrLRfgQx6X1CL%2FJ4Y8kYVptPx3wJGhVi0fPqnrV7Tn2Bv7%2BU99I46Om8NG%2Bhx8pJWPxbVRHIwW1llR3geV5S6cIVBtl7%2BijbYeDfcVSw4AUl0KHdMNs6MS2mY2fd9CcMV5ROXRBJ9mdYKO%2BuZROzG4Zk4ALpUN0FfdPbpz5aX4oIag4WPvSxUpMcSWpbQ4Nz9flhSmuTVlgltVn540oqRWP4xGpkW5NFC9N5BRyyKRc6X9LGxEG9ILTkwos%2FJugY6ngFtp0tjYYqxeJysqG8Aw9%2BRzPuS5%2FLITN3Mbqn7hOWcRgx%2BtJVVmu3AFzUfsYysyKyknPc25pdPi6mdts0Yfl0HUY%2FQij4KYL9PRqpVa7PmK4OB%2BtgmJsLKQOaoPyQhvjae0Yu39Dr%2B6h0nJscnbtexiY9zGAy7uNdqMwzfQuERnBFLpvmrgmOnbo8C8mhEz0q%2FHgPnNyhZg8800rWegQ%3D%3D&X-Amz-Signature=6652c2c933d5956d5a4ae24965251a0d4e31d3c32d3b9173ac25c495041b825d&X-Amz-SignedHeaders=host&x-id=GetObject';
   const text = `Hello ${params.user.name},\n\nYou have been added as a reviewer to an Earthdata Pub request.\nYour review can be added at ${envUrl}/dashboard/forms/id/${params.eventMessage.formId}?requestId=${params.eventMessage.submissionId}.`;
 
   const html = `
@@ -13,8 +41,7 @@ const getReviewerAddedTemplate = async (params, envUrl, xx) => {
                    <table>
                      <tr>
                       <td width="60">
-                        <!-- Replacing the URL with the Base64 string -->
-                        <img src="${base64Logo}" alt="Logo" title="Logo" style="display:block"/>                 
+                       ${imgTag}                      
                       </td>
                       <td>
                         <h4>Earthdata Pub</h4>
@@ -22,7 +49,7 @@ const getReviewerAddedTemplate = async (params, envUrl, xx) => {
                      </tr>
                    </table>
                  </td>
-                 <td align="right"><b>Reviewer Added</b>${xx}</td>
+                 <td align="right"><b>Reviewer Added</b></td>
                  <td></td>
              </tr>
              <tr>
