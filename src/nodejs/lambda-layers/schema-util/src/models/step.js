f503022bcc706a00d943fe279feb28bd6057b013
Dunivan Stahl, Chelsey K <dunivanck@ornl.gov>
2025-08-21 09:56:58 -0400

    updating step schema

--- src/nodejs/lambda-layers/schema-util/src/models/step.js
+++ src/nodejs/lambda-layers/schema-util/src/models/step.js
@@ -2,6 +2,7 @@ module.exports.model = (path) => ({
     description: 'A step used to describe the transition within workflows of',
     type: 'object',
     properties: {
+        step_id: { $ref: `#${path}UUID` },
         step_name: { type: 'string' },
         step_status_label: { type: 'string' },
         type: { type: 'string', enum: ['init', 'action', 'form', 'review', 'service', 'upload', 'close'] },
