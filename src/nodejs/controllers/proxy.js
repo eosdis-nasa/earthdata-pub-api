0abc77ec0cb4cb1c346919c4f69fe4cef7052763
Dunivan Stahl, Chelsey K <dunivanck@ornl.gov>
2025-11-07 10:13:10 -0500

    Updated worflow.findAll to use the sql-builder and to allow filtering options

--- src/nodejs/controllers/proxy.js
+++ src/nodejs/controllers/proxy.js
@@ -526,12 +526,10 @@ module.exports.workflowFindAll = function workflowFindAll(req, res, next) {
     resource: 'workflow',
     operation: 'findAll',
     params: {
-      query: {
-        sort: params.sort.value,
-        order: params.order.value,
-        per_page: params.per_page.value,
-        page: params.page.value
-      }
+      sort: params.sort.value,
+      order: params.order.value,
+      per_page: params.per_page.value,
+      page: params.page.value
     },
     context: { user_id: req.user_id }
   };
