47f487e1ea4545471e8f087fe61da8b70250bf84
camposeddie <eddie.campos@uah.edu>
2025-12-04 10:45:15 -0500

    Fix ordering of sql query where findAll is nested in findById

--- src/nodejs/lambda-layers/database-util/src/query/service.js
+++ src/nodejs/lambda-layers/database-util/src/query/service.js
@@ -1,16 +1,17 @@
 const sql = require('./sql-builder.js');
 
-const findAll = ({ sort, order, per_page, page }) => sql.select({
+const findAll = ({ sort, order, per_page, page, where }) => sql.select({
   fields: ['service.*'],
   from: { base: 'service' },
+  ...(where ? { where: {filters: [{cmd: where}]} }: {}),
   ...(sort ? { sort } : {}),
   ...(order ? { order } : {}),
   ...(per_page ? { limit: per_page } : {}),
   ...(page ? { offset: page } : {})
 });
 
-const findById = () => `${findAll()} WHERE service.id = {{id}}`;
-const findByName = () => `${findAll()} WHERE service.short_name = {{short_name}}`;
+const findById = () => findAll({where: 'service.id = {{id}}'});
+const findByName = () => findAll({where: 'service.short_name = {{short_name}}'});
 const createSecret = () => `INSERT INTO service_secret(id, secret, submission_id) VALUES ({{id}}, {{secret}}, {{submission_id}})`;
 const findSecret = () => `SELECT * FROM service_secret WHERE service_secret.id = {{id}} AND service_secret.submission_id = {{submissionId}}`;
 const deleteSecret = () => `DELETE FROM service_secret WHERE service_secret.id = {{serviceId}} AND service_secret.submission_id = {{submissionId}}`;
