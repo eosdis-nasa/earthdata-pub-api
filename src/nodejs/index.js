b13d84f9e9e7dce8fb3e40642c586eb144f63214
camposeddie <eddie.campos@uah.edu>
2026-02-26 16:08:11 -0500

    Test APIGateway update in SIT

--- src/nodejs/index.js
+++ src/nodejs/index.js
@@ -36,7 +36,8 @@ app.post('/goaws/workflow_consumer', local.handleWorkflow);
 app.post('/goaws/metrics_consumer', local.handleMetrics);
 app.post('/goaws/notification_consumer', local.handleNotification);
 app.post('/database_test', local.dbTest);
-app.get('/docs', (req, res) => res.redirect('/docs/index.html'));
+app.get('/docs', (req, res) => res.redirect('/docs/swagger-index.html'));
+app.get('/docs/index.html', (req, res) => res.redirect('/docs/swagger-index.html'));
 app.get('/api-docs', (req, res) => res.redirect('/api-docs/index.html'));
 
 const spec = fs.readFileSync(path.join(__dirname, '/api/openapi.json'), 'utf8');
